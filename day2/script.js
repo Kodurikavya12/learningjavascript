eb93877fdfffbd69e51339120f33db97f8918084
Kodurikavya12 <kodurikavya30@gmail.com>
2026-04-11 09:28:18 +0530

    files added

---
+++ day2/script.js
@@ -0,0 +1,20 @@
+// console.log(-1)
+
+// var a=10;
+// var a=20;
+// console.log(a);
+
+// const b=15;
+// console.log(b);
+
+
+// let c=25;
+// c=33;
+// console.log(c);
+
+//Arrays
+let userData = [10, "hello", false, 9.11, 'h'];
+for(let index=0; index< userData.length;index++){
+    console.log(typeof userData[index]);
+    
+}
