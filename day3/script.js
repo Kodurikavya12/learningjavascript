5874cb6f2215609a5f08dada0c2b6b10fc65c730
Kodurikavya12 <kodurikavya30@gmail.com>
2026-04-13 09:36:48 +0530

    day3 added

--- day3/script.js
+++ day3/script.js
@@ -9,16 +9,42 @@
 // console.log(obj);
 
 
-const users =[
-    {
-    id:1,
-    name : "user1",
-    loc : "hyd"
-    },
-    {
-        id:2,
-        name:"user2",
-        loc:"hyd"
-    }
-]
-console.log(users[0].name);
+// const users =[
+//     {
+//     id:1,
+//     name : "user1",
+//     loc : "hyd"
+//     },
+//     {
+//         id:2,
+//         name:"user2",
+//         loc:"hyd"
+//     }
+// ]
+// console.log(users[0].name);
+
+// function myFun() {
+//     // console.log(-1);
+//     return -1
+        
+// }
+// console.log(myFun);
+// console.log(myFun());
+
+// function add(num1, num2){
+//     return num1+num2
+// }
+// console.log(add(32,2));
+
+// function add(num1, num2){
+//     return num1-num2
+// }
+// console.log(add(36, 6));
+
+const myFun=function add(num1,num2){
+    return num1+num2
+}
+console.log(myFun(33,3));
+
+
+ 
