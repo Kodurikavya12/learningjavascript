b1e74fd786fa2b74f75e56a8210bc3216c16951d
Kodurikavya12 <kodurikavya30@gmail.com>
2026-04-16 09:55:24 +0530

    file added

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 const add=() => {
     event.preventDefault()
     let val1=Number(document.getElementById("value1").value)
-    let val2=document.getElementById("value2")
+    let val2=Number(document.getElementById("value2").value)
     let display=document.getElementById("display")
 
-    display.innerHTML=`The sum of ${val1} and ${val2.value} is ${val1 + val2.value}`
+    display.innerHTML=`The sum of ${val1} and ${val2} is ${val1 + val2}`
 }
 const sub=() => {
     event.preventDefault()
