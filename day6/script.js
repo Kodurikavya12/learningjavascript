075351d89ab5805a012d0778d2c6b76024d03598
Kodurikavya12 <kodurikavya30@gmail.com>
2026-04-17 09:17:17 +0530

    file added

---
+++ day6/script.js
@@ -0,0 +1,12 @@
+const changeColor = () => {
+    let display = document.getElementById("display")
+    let color= "#"
+    let colorCodes=[0,1,2,3,4,5,6,7,8,9, 'a', 'b', 'c', 'd', 'e','f']
+
+    for(let i=0;i<6;i++){
+        let randNo = Math.floor(Math.random()*16)
+        color += colorCodes[randNo]
+    }
+    display.style.backgroundColor = colorCodes
+}
+changeColor()
