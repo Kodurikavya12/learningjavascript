2342910626f52778e0a403e02a95acb2aca427a0
Kodurikavya12 <kodurikavya30@gmail.com>
2026-04-22 09:40:53 +0530

    day4 added

---
+++ day4/script.js
@@ -0,0 +1,26 @@
+// call backs
+
+// function myFun() {
+//     console.log("Hello");
+    
+// }
+// setTimeout(myFun, 2000)
+
+// setInterval(myFun, 2000)
+
+const fetchAPI = async () => {
+    let imageURL = null;
+
+    try{
+        let res = await fetch("https://dog.ceo/api/breeds/image/random")
+        let data = await res.json()
+        imageURL = data.message
+    }
+    catch (err) {
+        console.log(err);
+        
+    }
+    let image = document.getElementById("image")
+    image.src = imageURL
+}
+fetchAPI()
